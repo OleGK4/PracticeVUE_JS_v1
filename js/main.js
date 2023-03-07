3e9c598088ea3b36733c500a77287637f56b52db
gr411_kaoev <oleg.krut.64@gmail.com>
2023-03-07 12:55:49 +0700

    v3 Удаление, добавление, новый список

--- js/main.js
+++ js/main.js
@@ -1,43 +1,95 @@
 let product = "Socks";
 
-let app = new Vue({
 
-    el: '#app',
-    data: {
-        product: "НАСКИ",
-        brand: 'Вуе Ауе',
-        selectedVariant: 0,
-        altText: "ПАРА НАСКОВ",
-        // inStock: false,
-        inventory: 100,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10,
-                variantOnSale: true,
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0,
-                variantOnSale: false,
-            }
-        ],
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+   <div class="product">
+    <div class="product-image">
+            <img alt="#" :src="image" :alt="altText"/>
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p class="product-info" v-if="inStock === false" :class="{ productInfoOutOfStock: !inStock }">Out of stock!</p>
+            <p class="product-info" v-else-if="inventory > 10">In stock</p>
+            <p class="product-info" v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+            <p class="product-info" v-else :class="{ productInfoOutOfStock: !inStock }">Out of stock</p>
+            <product-detail></product-detail>
+            <p>Shipping: {{ shipping }}</p>
+
+            <div
+                 class="color-box"
+                 v-for="(variant, index) in variants"
+                 :key="variant.variantId"
+                 :style="{ backgroundColor:variant.variantColor }"
+                 @mouseover="updateProduct(index)"
+            >
+            </div>
+
+            <ul>
+                <li v-for="sizes in sizes">{{ sizes }}</li>
+            </ul>
+
+            <h2>{{ sale }}</h2>
 
 
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0
+            <button
+                    v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+            >
+                Add to cart
+            </button>
+            <button class="button-delete" v-on:click="delFromCart">Del from cart</button>
+
+         
+
+
+
+        </div>
+   </div>
+ `,
+    data() {
+        return {
+            product: "НАСКИ",
+            brand: 'Вуе Ауе',
+            selectedVariant: 0,
+            altText: "ПАРА НАСКОВ",
+            // inStock: false,
+            inventory: 100,
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10,
+                    variantOnSale: true,
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 10,
+                    variantOnSale: false,
+                }
+            ],
+
+
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: []
+        }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
         delFromCart() {
-            this.cart -= 1
+            this.$emit('del-from-cart', this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index) {
             this.selectedVariant = index;
@@ -52,52 +104,58 @@ let app = new Vue({
         image() {
             return this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
         sale() {
-            if (this.variants[this.selectedVariant].variantOnSale === true){
+            if (this.variants[this.selectedVariant].variantOnSale === true) {
                 return ' ON SALE!'
-            }else {
+            } else {
                 return '';
             }
-        }
-
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99
+            }
+        },
     }
 })
 
-Vue.component('product', {
-    template: `
-   <div class="product">
 
-   </div>
- `,
+Vue.component('product-detail', {
     data() {
         return {
-            // тут будут данные
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         }
     },
-    methods: {
-        // тут будут методы
+    template: `
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `,
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true,
+        cart: []
     },
-    computed: {
-        // тут будут вычисляемые свойства
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        deleteFromCart(id) {
+            this.cart.shift(id);
+        }
+
     }
+
 })
 
 
 
-
-// let app2 = new Vue({
-//
-//     el: '#app2',
-//     data: {
-//         product: "НАСКИ ЗЕЛЕНЬ",
-//         image: "./assets/vmSocks-green-onWhite.jpg",
-//         altText: "ПАРА НАСКОВ",
-//         inStock: true,
-//         inventory: 10,
-//         details: ['80% cotton', '20% polyester', 'Gender-neutral']
-//     }
-// })
 
